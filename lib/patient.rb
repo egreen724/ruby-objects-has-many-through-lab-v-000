b4a623741df61cf55be35443cf20514ec24b2f46
Erin Green <erin.y.green@gmail.com>
2018-10-11 01:52:46 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,3 +1,17 @@
 class Patient
   
+  attr_accessor :all 
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self 
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  
 end
