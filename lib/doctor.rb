02f55b820b22dd83d69a868837f4b49eed18d662
Erin Green <erin.y.green@gmail.com>
2018-10-11 01:59:38 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -16,7 +16,7 @@ class Doctor
   def new_appointment(patient, date)
     Appointment.new(patient, date, self)
   end
-  
+
   def appointments 
     Appointment.all.collect do |appointment|
       appointment.doctor == self 
